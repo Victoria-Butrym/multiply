affeb23f58052c55a00af88ae939273fe0955461
Victoria-Butrym <victoria-butrym@tut.by>
2019-09-26 13:00:38 +0300

    feat: add solution using BigInt function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function multiply(first, second) {
   // your solution
+  first = BigInt(first);
+  second = BigInt(second);
+  let mult = (first * second).toString();  
+  return mult;
 }
